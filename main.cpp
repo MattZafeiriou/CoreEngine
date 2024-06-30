15ce38668b477b1aaf54e5e82cc101a0571b6cf4
MattZafeiriou <matuaioszafeiriou@gmail.com>
2024-07-01 00:59:41 +0300

    Add start files, update gitignore

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // for Mac OS X
+
+    /*
+	 * Create a windowed mode window and its OpenGL context
+	*/
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Niggatron", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    /*
+     * Initialize GLAD before calling any OpenGL function
+    */
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    /*
+	 * Render loop
+	*/
+    while(!glfwWindowShouldClose(window))
+	{
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+	}
+
+    glfwTerminate();
+    return 0;
+}
